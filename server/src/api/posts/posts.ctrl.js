62da0b229050c3a6206524527a1027af5500f830
wjdwndtlr <wjdwndtlr1024@gmail.com>
2020-05-25 17:59:18 +0900

    feat: Server posts.ctrl 기능완성

--- server/src/api/posts/posts.ctrl.js
+++ server/src/api/posts/posts.ctrl.js
@@ -47,9 +47,29 @@ export const write = async (ctx) => {
 
 // 데이터 조회
 export const list = async (ctx) => {
+  // query는 문자열이기 때문에 숫자로 변환해 줘야함
+  // 값이 없다면 1을 기본으로 사용
+  const page = parseInt(ctx.query.page || '1', 10);
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
   try {
-    const posts = await Post.find().exec();
-    ctx.body = posts;
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(8)
+      .skip((page - 1) * 8)
+      .lean()
+      .exec();
+
+    const postCount = await Post.countDocuments().exec();
+    ctx.set('Last-Page', Math.ceil(postCount / 8));
+    ctx.body = posts.map((post) => ({
+      ...post,
+      body:
+        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+    }));
   } catch (e) {
     ctx.throw(500, e);
   }
