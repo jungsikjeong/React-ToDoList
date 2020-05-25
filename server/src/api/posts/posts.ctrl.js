de6a7ebea391b4c6441a1b2719b842eacb330884
wjdwndtlr <wjdwndtlr1024@gmail.com>
2020-05-25 15:34:11 +0900

    feat: Server write기능 추가

--- server/src/api/posts/posts.ctrl.js
+++ server/src/api/posts/posts.ctrl.js
@@ -1,3 +1,5 @@
+import Post from '../../models/post';
+
 let postId = 1;
 
 const posts = [
@@ -8,12 +10,19 @@ const posts = [
   },
 ];
 
-export const write = (ctx) => {
-  const { title, body } = ctx.request.body;
-  postId += 1;
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
+export const write = async (ctx) => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+
+  try {
+    await post.save();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const list = (ctx) => {
@@ -48,28 +57,6 @@ export const remove = (ctx) => {
   }
   posts.splice(index, 1);
   ctx.status = 204; // No Content
-};
-
-/** 포스트 수정(교체) */
-export const replace = (ctx) => {
-  const { id } = ctx.params;
-
-  const index = posts.findIndex((p) => p.id.toString() === id);
-
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
-  }
-  // 전체 객체를 덮어 씌움
-  // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만듦
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
 };
 
 /** 포스트 수정(특정 필드 변경) */
