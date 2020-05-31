6fa652f1461ad88b813235f09cfda3ffc90142d9
wjdwndtlr <wjdwndtlr1024@gmail.com>
2020-05-31 15:02:38 +0900

    Add:Histroy 구현

--- client/src/containers/auth/RegisterForm.js
+++ client/src/containers/auth/RegisterForm.js
@@ -3,8 +3,9 @@ import AuthForm from '../../components/auth/AuthForm';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, register } from '../../modules/auth';
 import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
-const RegisterForm = () => {
+const RegisterForm = ({ history }) => {
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
@@ -58,10 +59,9 @@ const RegisterForm = () => {
   // user 값이 잘 설정되었는지 확인
   useEffect(() => {
     if (user) {
-      console.log('check API 성공');
-      console.log(user);
+      history.push('/');
     }
-  }, [user]);
+  }, [history, user]);
   return (
     <AuthForm
       type="register"
@@ -72,4 +72,4 @@ const RegisterForm = () => {
   );
 };
 
-export default RegisterForm;
+export default withRouter(RegisterForm);
