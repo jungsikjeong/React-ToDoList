8d458d9a3b18b4a0fd83191936afed06ba85c76f
wjdwndtlr <wjdwndtlr1024@gmail.com>
2020-06-01 18:15:19 +0900

    Add: Header 유저 박스 작업

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { keyframes, css } from 'styled-components';
 import Responsive from './Responsive';
 import Button from './Button';
 import { Link } from 'react-router-dom';
 import { FaUserCircle } from 'react-icons/fa';
 
+const infoFrame = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(-10px);
+    }
+  100% {
+    opacity: 1;
+    transform: translateY(0px);
+    }
+`;
+
 const HeaderBlock = styled.div`
   position: fixed;
   width: 100%;
-  /* background: white; */
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
 `;
 
@@ -26,6 +36,10 @@ const Wrapper = styled(Responsive)`
   .right {
     display: flex;
     align-items: center;
+
+    .userLogo {
+      cursor: pointer;
+    }
   }
 `;
 
@@ -34,16 +48,93 @@ const SLink = styled(Link)`
     opacity: 0.7;
   }
 `;
+
+const InfoLink = styled(Link)`
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  background-color: transparent;
+  color: rgb(51, 51, 51);
+  padding: 0px 36px 0px 16px;
+  :hover {
+    background: #7095b3;
+  }
+`;
+
+const LogoutBtn = styled(Button)`
+  display: flex;
+  width: 100%;
+  height: 40px;
+  border: none;
+  background-color: transparent;
+  color: rgb(51, 51, 51);
+  font-size: 14px;
+  font-weight: unset;
+  padding: 0px 36px 0px 16px;
+  :hover {
+    background: #7095b3;
+  }
+`;
+
 const Spacer = styled.div`
   height: 4rem;
 `;
 
 const UserInfo = styled.div`
+  color: white;
   font-weight: 600;
   margin-right: 1rem;
+  cursor: pointer;
+`;
+const SubInfoBlock = styled.div`
+  display: block;
+  position: absolute;
+  top: -59px;
+  right: 122px;
+  margin: 110px 0px 0px;
+  font-size: 14px;
+  animation: 0.3s ease 0s 1 normal forwards running ${infoFrame};
 `;
 
-const Header = ({ user }) => {
+const SubInfoWrapper = styled.div`
+  width: 222px;
+  height: 145px;
+  border-radius: 10px;
+  background-color: rgb(186, 224, 255);
+  box-shadow: rgba(0, 0, 0, 0.75) 0px 0px 15px 2px;
+  .userInfo {
+    display: flex;
+    width: 100%;
+    height: 60px;
+    background-color: transparent;
+    border: 10px;
+    padding: 10px;
+    border-bottom: 1px solid rgb(87, 138, 179);
+
+    .column img {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+    }
+    .column:last-child {
+      display: flex;
+      flex-direction: column;
+      margin-left: 10px;
+    }
+    .column .userName {
+      font-size: 16px;
+      font-weight: 500;
+    }
+    .column .userEmail {
+      font-size: 14px;
+      margin-top: 5px;
+    }
+  }
+`;
+
+const Header = ({ user, onToggle, toggle }) => {
   return (
     <>
       <HeaderBlock>
@@ -53,7 +144,34 @@ const Header = ({ user }) => {
           </div>
           {user ? (
             <div className="right">
-              <UserInfo>{user.name}</UserInfo>
+              <FaUserCircle
+                size="30"
+                color="#3498db"
+                className="userLogo"
+                onClick={() => onToggle()}
+              />
+              {toggle && (
+                <SubInfoBlock>
+                  <SubInfoWrapper>
+                    <div className="userInfo">
+                      <div className="column">
+                        <img
+                          src="https://nomad-hackathon-2020.s3.ap-northeast-2.amazonaws.com/default_image.png"
+                          alt="avatar"
+                        />
+                      </div>
+                      <div className="column">
+                        <span className="userName">{user.name}</span>
+                        <span className="userEmail">{user.email}</span>
+                      </div>
+                    </div>
+                    <InfoLink to="/" className="infoLink">
+                      내 정보
+                    </InfoLink>
+                    <LogoutBtn>로그아웃</LogoutBtn>
+                  </SubInfoWrapper>
+                </SubInfoBlock>
+              )}
             </div>
           ) : (
             <div className="right">
--- client/src/containers/common/HeaderContainer.js
+++ client/src/containers/common/HeaderContainer.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/common/Header';
 import { useSelector } from 'react-redux';
 
 const HeaderContainer = () => {
+  const [toggle, setToggle] = useState(false);
+
   const { user } = useSelector(({ user }) => ({ user: user.user }));
-  return <Header user={user} />;
+
+  const onToggle = () => {
+    setToggle(!toggle);
+  };
+  return <Header user={user} onToggle={onToggle} toggle={toggle} />;
 };
 
 export default HeaderContainer;
